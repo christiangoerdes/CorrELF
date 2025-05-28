d4f0165de98989a82074af0c52455b3d446f6dd4
Christian Gördes <christian.goerdes@outlook.de>
2025-05-28 15:38:54 +0200

    Optimize file analysis by streamlining entity saving logic and disabling JPA SQL logging

--- src/main/java/com/goerdes/correlf/services/FileAnalysisService.java
+++ src/main/java/com/goerdes/correlf/services/FileAnalysisService.java
@@ -15,7 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.goerdes.correlf.handler.ElfHandler.createEntity;
 import static com.goerdes.correlf.handler.ElfHandler.fromMultipart;
