14b70b7e0379771a548d07f3994d2afca7a3d5f9
Christian Gördes <christian.goerdes@outlook.de>
2025-06-03 18:51:47 +0200

    Refactor ElfHandlerTest to use ElfWrapper instead of fromMultipart for entity creation

--- src/test/java/com/goerdes/correlf/ElfHandlerTest.java
+++ src/test/java/com/goerdes/correlf/ElfHandlerTest.java
@@ -2,20 +2,20 @@ package com.goerdes.correlf;
 
 import com.goerdes.correlf.components.ElfHandler;
 import com.goerdes.correlf.components.MinHashProvider;
+import com.goerdes.correlf.model.ElfWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
 import static com.goerdes.correlf.TestUtils.getMockFile;
-import static com.goerdes.correlf.components.ElfHandler.fromMultipart;
 
 @SpringBootTest
 public class ElfHandlerTest {
 
     @Test
     void testEntityCreation() throws IOException {
-        System.out.println(new ElfHandler(new MinHashProvider()).createEntity(fromMultipart(getMockFile("busybox"))));
+        System.out.println(new ElfHandler(new MinHashProvider()).createEntity(new ElfWrapper(getMockFile("busybox"))));
     }
 
 }
